feee13aecaaab4563a97443549bfa684279e4214
aefuente <aefuente@pm.me>
2025-05-28 16:02:34 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# md-parser
+# Markdown Parser
+
+A simple markdown to pdf translator written in the Zig programming language.
+
+## Mission Statement
+
+I often find myself taking notes in markdown. Sometimes these notes are useful
+for non-technical users. I wanted to be able to translate markdown into a format
+that is consumable via a standalone file that non-technical people could easily
+read and wouldn't find concerning to open. I want this file format to be
+viewable over preview mechanisms such as Teams, Slack and OneDrive.
---
+++ src/rules.zig
@@ -0,0 +1,50 @@
+//! This module holds a list of utilities and rules for the tokenization process
+//! of Markdown
+
+const std = @import("std");
+const mvzr = @import("mvzr.zig");
+
+
+/// Structure for holding the state to find \r\n characters
+const windows_endline_normalize = enum {
+
+    /// S for Start R for \r
+    S, R
+};
+
+/// Function to replace all \r\n to just \n
+pub fn normalize(data: *std.ArrayList(u8)) !void {
+    var state = windows_endline_normalize.S;
+    var index: usize =  0;
+    while (index < data.items.len) {
+        switch (state) {
+            .S => {
+                if (data.items[index] == '\r') {
+                    state = windows_endline_normalize.R;
+                }
+
+            },
+            .R => {
+                if (data.items[index] == '\n') {
+                    _ = data.orderedRemove(index-1);
+                    state = windows_endline_normalize.S;
+                    continue;
+                }
+                state = windows_endline_normalize.S;
+            },
+        }
+        index += 1;
+    }
+}
+
+
+test "normalize line breaks" {
+    const allocator = std.testing.allocator;
+    const test_string1 = "This is my test string\r\nIt's\r\ngoing\rwell\n";
+    var data = std.ArrayList(u8).init(allocator);
+    defer data.deinit();
+    try data.appendSlice(test_string1);
+    try normalize(&data);
+    try std.testing.expectEqualStrings(data.items, "This is my test string\nIt's\ngoing\rwell\n");
+
+}
